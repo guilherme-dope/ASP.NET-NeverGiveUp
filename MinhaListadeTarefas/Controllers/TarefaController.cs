de9edc693bd025093ad27a04396962b9ebe379d9
guilherme-dope <guimontemor777@gmail.com>
2025-07-10 20:59:58 -0300

    Created TarefaController and Tarefa View Index

---
+++ MinhaListadeTarefas/Controllers/TarefaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaListadeTarefas.Controllers
+{
+    public class TarefaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
