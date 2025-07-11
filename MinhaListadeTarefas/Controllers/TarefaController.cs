0533b1a31ae03f8d2931da1b1b421f7c80b8315a
guilherme-dope <guimontemor777@gmail.com>
2025-07-10 23:42:53 -0300

    Enhance task management features in TarefaController
    Updated TarefaController to populate task lists in Index and Edit actions.
    
    Added nuget package reference for code generation in the project file (Scaffolding).
    
    Enhanced Index.cshtml to display tasks in a table format with action buttons.
    
    Added Edit.cshtml view for editing task details with validation.

--- MinhaListadeTarefas/Controllers/TarefaController.cs
+++ MinhaListadeTarefas/Controllers/TarefaController.cs
@@ -7,7 +7,13 @@ namespace MinhaListadeTarefas.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var list = new List<Tarefa>();
+            list.Add(new Tarefa { Id = 1, Descricao = "Tarefa 1", DataInicio = DateTime.Now, DataFim = DateTime.Now.AddDays(1) });
+            list.Add(new Tarefa { Id = 2, Descricao = "Tarefa 2", DataInicio = DateTime.Now.AddDays(1), DataFim = DateTime.Now.AddDays(2) });
+            list.Add(new Tarefa { Id = 3, Descricao = "Tarefa 3", DataInicio = DateTime.Now.AddDays(2), DataFim = DateTime.Now.AddDays(3) });
+            list.Add(new Tarefa { Id = 4, Descricao = "Tarefa 4", DataInicio = DateTime.Now.AddDays(3), DataFim = DateTime.Now.AddDays(4) });
+
+            return View(list);
         }
 
         [HttpGet]
@@ -32,5 +38,19 @@ namespace MinhaListadeTarefas.Controllers
             return View();
         }
 
+        public IActionResult Edit(int id)
+        {
+
+            var list = new List<Tarefa>();
+            list.Add(new Tarefa { Id = 1, Descricao = "Tarefa 1", DataInicio = DateTime.Now, DataFim = DateTime.Now.AddDays(1) });
+            list.Add(new Tarefa { Id = 2, Descricao = "Tarefa 2", DataInicio = DateTime.Now.AddDays(1), DataFim = DateTime.Now.AddDays(2) });
+            list.Add(new Tarefa { Id = 3, Descricao = "Tarefa 3", DataInicio = DateTime.Now.AddDays(2), DataFim = DateTime.Now.AddDays(3) });
+            list.Add(new Tarefa { Id = 4, Descricao = "Tarefa 4", DataInicio = DateTime.Now.AddDays(3), DataFim = DateTime.Now.AddDays(4) });
+
+            // Simulate fetching the task from a database
+            var tarefa = (from p in list where p.Id == id select p).FirstOrDefault();
+
+            return View(tarefa);
+        }
     }
 }
