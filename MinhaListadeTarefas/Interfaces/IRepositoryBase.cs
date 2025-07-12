634d77995d9bbfa1c602bf9e37333632ef865664
guilherme-dope <guimontemor777@gmail.com>
2025-07-12 15:14:12 -0300

    Created RepositoryBase Interface.

---
+++ MinhaListadeTarefas/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,33 @@
+﻿namespace MinhaListadeTarefas.Interfaces
+{
+    public interface IRepositoryBase
+    {
+       
+
+        public interface IRepositoryBase<T> where T : class
+        {
+            // Async methods for CRUD operations
+
+            Task<T> SelecionarChaveAsync(params object[] variavel);
+            Task<T> IncluirAsync(T entity);
+            Task<T> AlterarAsync(T entity);
+            Task<List<T>> ListarTodosAsync();
+            Task ExcluirAsync(T entity);
+
+            Task ExcluirAsync(int id);
+
+            // Synchronous methods for CRUD operations
+
+            T SelecionarChave(params object[] variavel);
+            T Incluir(T entity);
+            T Alterar(T entity);
+            List<T> ListarTodos();
+            void Excluir(T entity);
+            void Excluir(int id);
+
+
+
+
+        }
+    }
+}
