603afd6355ded9880a77b2d2f7582ee85ffd4cb2
guilherme-dope <guimontemor777@gmail.com>
2025-07-13 18:15:26 -0300

    Created Services and ServiceTarefa

---
+++ MinhaListadeTarefas/Services/ServiceTarefa.cs
@@ -0,0 +1,28 @@
+﻿using MinhaListadeTarefas.Models;
+using MinhaListadeTarefas.Repositories;
+
+namespace MinhaListadeTarefas.Services
+{
+    public class ServiceTarefa
+    {
+        private AppDbContext _context;
+
+        public RepositoryTarefa RptTarefa { get; set; }
+        public RepositoryCategoria RptCategoria { get; set; }
+        public RepositoryPrioridade RptPrioridade { get; set; }
+        public RepositoryResponsavel RptResponsavel { get; set; }
+        public RepositoryStatus RptStatus { get; set; }
+
+        public ServiceTarefa(AppDbContext context)
+        {
+            _context = context;
+            RptTarefa = new RepositoryTarefa(_context);
+            RptCategoria = new RepositoryCategoria(_context);
+            RptPrioridade = new RepositoryPrioridade(_context);
+            RptResponsavel = new RepositoryResponsavel(_context);
+            RptStatus = new RepositoryStatus(_context);
+
+        }
+
+    }
+}
